93f8d91848b86907c2c22c05448f26dee6924a11
Coducteur <chat.adr01@gmail.com>
2023-01-09 12:13:30 +0100

    modifs demandées

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'newFirstProject';
+  title = "Bienvenue sur le site d'Adrien !";
 }
